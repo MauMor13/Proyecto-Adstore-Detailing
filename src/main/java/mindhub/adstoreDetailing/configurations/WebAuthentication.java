aeb34bf8a6a44a1b02fe2429b1c053fd48008766
RicardoSanchezOrtiz <ricardosanchez1997@hotmail.com>
2023-03-23 16:55:25 -0500

    Agregando autorizacion y autenticacion

---
+++ src/main/java/mindhub/adstoreDetailing/configurations/WebAuthentication.java
@@ -0,0 +1,36 @@
+package mindhub.adstoreDetailing.configurations;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class WebAuthentication extends GlobalAuthenticationConfigurerAdapter {
+//    @Autowired
+//    ClienteRepositorio clienteRepo;
+
+    @Override
+    public void init(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(email ->{
+            Cliente cliente = clienteRepo.findByEmail(email);
+            if (cliente.getEmail().equalsIgnoreCase("admin@storage.com")){
+                return new User(cliente.getEmail(), passwordEncoder().encode(cliente.getContrasenia()) , AuthorityUtils.createAuthorityList("ADMIN"));
+            } else if (cliente != null){
+                return new User(cliente.getEmail(), cliente.getContrasenia(), AuthorityUtils.createAuthorityList("MIEMBRO"));
+            }
+            else{
+                throw new UsernameNotFoundException("Usuario desconocido o no registrado" +  email);
+            }
+        });
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
