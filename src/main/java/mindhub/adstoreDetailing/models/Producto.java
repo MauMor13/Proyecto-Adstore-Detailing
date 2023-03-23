9c5c1f37e39603f0193d13a5e105946a941eca90
SLautaro <111198390+LautaroNSantillan@users.noreply.github.com>
2023-03-23 19:30:05 -0300

    algunos modelos y repos

---
+++ src/main/java/mindhub/adstoreDetailing/models/Producto.java
@@ -0,0 +1,27 @@
+package mindhub.adstoreDetailing.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "producto_seq")
+    @SequenceGenerator(name = "producto_seq", sequenceName = "producto_id_seq", allocationSize = 1)
+    private long id;
+    private String productoNombre;
+    private double precio;
+    private int stock;
+    private String descripcion;
+    private int descuento;
+    private String imagenURL;
+
+}
---
+++ src/main/java/mindhub/adstoreDetailing/repositorios/RepositorioServicio.java
@@ -0,0 +1,8 @@
+package mindhub.adstoreDetailing.repositorios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface RepositorioServicio extends JpaRepository {
+}
