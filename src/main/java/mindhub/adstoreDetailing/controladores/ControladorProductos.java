1f245844f05a93dcd611afb323447f3d20e080a7
SLautaro <111198390+LautaroNSantillan@users.noreply.github.com>
2023-03-27 20:50:04 -0300

    servicio producto

--- src/main/java/mindhub/adstoreDetailing/controladores/ControladorProductos.java
+++ src/main/java/mindhub/adstoreDetailing/controladores/ControladorProductos.java
@@ -1,8 +1,7 @@
 package mindhub.adstoreDetailing.controladores;
 
 import mindhub.adstoreDetailing.dtos.ProductoDTO;
-import mindhub.adstoreDetailing.repositorios.RepositorioProducto;
-import org.springframework.beans.factory.annotation.Autowired;
+import mindhub.adstoreDetailing.servicios.ServicioProducto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,11 +12,13 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/api")
 public class ControladorProductos {
-    @Autowired
-    private RepositorioProducto repositorioProducto;
+   private final ServicioProducto servicioProducto;
+    public ControladorProductos(ServicioProducto servicioProducto) {
+        this.servicioProducto = servicioProducto;
+    }
 
     @GetMapping("/productos")
     public List<ProductoDTO> traerProductos(){
-        return repositorioProducto.findAll().stream().map(ProductoDTO::new).collect(Collectors.toList());
+        return servicioProducto.findAllDTOs();
     }
 }
