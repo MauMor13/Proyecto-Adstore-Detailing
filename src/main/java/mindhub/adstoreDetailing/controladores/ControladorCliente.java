3fa6198f3b04bfb0c20c345d500b81362bc6326e
SLautaro <111198390+LautaroNSantillan@users.noreply.github.com>
2023-03-27 18:23:13 -0300

    placeholder

--- src/main/java/mindhub/adstoreDetailing/controladores/ControladorCliente.java
+++ src/main/java/mindhub/adstoreDetailing/controladores/ControladorCliente.java
@@ -9,40 +9,39 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//@RestController
-//@RequestMapping("/api")
-//public class ControladorCliente {
-//    private final ServicioCliente servicioCliente;
-//    public ControladorCliente(ServicioCliente servicioCliente) {
-//        this.servicioCliente = servicioCliente;
-//    }
-//
-//    @PostMapping("/register")
-//    public ResponseEntity<Object> register(@RequestBody Cliente cliente) {
-//
-//        if (incomingObj.getFirstName().isEmpty() ) {
-//            return new ResponseEntity<>("Missing First Name", HttpStatus.BAD_REQUEST);
-//        }
-//        if (incomingObj.getLastName().isEmpty() ) {
-//            return new ResponseEntity<>("Missing Last Name", HttpStatus.BAD_REQUEST);
-//        }
-//        if (incomingObj.getEmail().isEmpty()) {
-//            return new ResponseEntity<>("Missing Email", HttpStatus.BAD_REQUEST);
-//        }
-//        else if (incomingObj.getEmail().endsWith("@admin.mindhub")) {
-//            return new ResponseEntity<>("Invalid Email, you can't use this email", HttpStatus.BAD_REQUEST);
-//        }
-//        if (incomingObj.getPassword().isEmpty() ) {
-//            return new ResponseEntity<>("Missing Password", HttpStatus.BAD_REQUEST);
-//        }
-//
-//        if (clientService.findByEmail(incomingObj.getEmail()) !=  null) {
-//            return new ResponseEntity<>("Email already in use", HttpStatus.FORBIDDEN);
-//        }
-//
-//        clientService.saveNewClient(cliente);
-//        return new ResponseEntity<>("Logged in",HttpStatus.CREATED);
-//    }
-//
-//
-//}
+@RestController
+@RequestMapping("/api")
+public class ControladorCliente {
+    private final ServicioCliente servicioCliente;
+    public ControladorCliente(ServicioCliente servicioCliente) {
+        this.servicioCliente = servicioCliente;
+    }
+
+    @PostMapping("/registrar")
+    public ResponseEntity<Object> register(@RequestBody Cliente cliente) {
+
+        if (cliente.getNombre().isEmpty()) {
+            return new ResponseEntity<>("Ingrese Nombre", HttpStatus.BAD_REQUEST);
+        }
+        if (cliente.getApellido().isEmpty()) {
+            return new ResponseEntity<>("Ingrese apellido", HttpStatus.BAD_REQUEST);
+        }
+        if (cliente.getEmail().isEmpty()) {
+            return new ResponseEntity<>("Missing Email", HttpStatus.BAD_REQUEST);
+        }
+        else if (cliente.getEmail().endsWith("@admin.adstore") || servicioCliente.emailEsValido(cliente.getEmail())) {
+            return new ResponseEntity<>("Invalid Email, you can't use this email", HttpStatus.BAD_REQUEST);
+        }
+        if (cliente.getClaveIngreso().isEmpty() ) {
+            return new ResponseEntity<>("Missing Password", HttpStatus.BAD_REQUEST);
+        }
+        if (servicioCliente.findByEmail(cliente.getEmail()) !=  null) {
+            return new ResponseEntity<>("Email already in use", HttpStatus.FORBIDDEN);
+        }
+
+        servicioCliente.registrarCliente(cliente);
+        return new ResponseEntity<>("Registrado",HttpStatus.CREATED);
+    }
+
+
+}
