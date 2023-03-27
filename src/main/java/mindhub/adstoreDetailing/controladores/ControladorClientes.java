5edae6edfaa81b2b5b1ba8c0d029fefe11d20a20
SLautaro <111198390+LautaroNSantillan@users.noreply.github.com>
2023-03-27 20:31:02 -0300

    traer cliente y productos

--- src/main/java/mindhub/adstoreDetailing/controladores/ControladorClientes.java
+++ src/main/java/mindhub/adstoreDetailing/controladores/ControladorClientes.java
@@ -1,24 +1,28 @@
 package mindhub.adstoreDetailing.controladores;
 
+import mindhub.adstoreDetailing.dtos.ClienteDTO;
 import mindhub.adstoreDetailing.models.Cliente;
 import mindhub.adstoreDetailing.servicios.ServicioCliente;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
-public class ControladorCliente {
+public class ControladorClientes {
     private final ServicioCliente servicioCliente;
-    public ControladorCliente(ServicioCliente servicioCliente) {
+    public ControladorClientes(ServicioCliente servicioCliente) {
         this.servicioCliente = servicioCliente;
     }
 
+    @GetMapping("/cliente")
+    public ClienteDTO traerCliente(Authentication auth){
+        return new ClienteDTO(this.servicioCliente.findByEmail(auth.getName()));
+    }
+
     @PostMapping("/registrar")
-    public ResponseEntity<Object> register(@RequestBody Cliente cliente) {
+    public ResponseEntity<Object> registrar(@RequestBody Cliente cliente) {
 
         if (cliente.getNombre().isEmpty()) {
             return new ResponseEntity<>("Ingrese Nombre", HttpStatus.BAD_REQUEST);
@@ -27,16 +31,16 @@ public class ControladorCliente {
             return new ResponseEntity<>("Ingrese apellido", HttpStatus.BAD_REQUEST);
         }
         if (cliente.getEmail().isEmpty()) {
-            return new ResponseEntity<>("Missing Email", HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>("Ingrese Email", HttpStatus.BAD_REQUEST);
         }
         else if (cliente.getEmail().endsWith("@admin.adstore") || servicioCliente.emailEsValido(cliente.getEmail())) {
             return new ResponseEntity<>("Invalid Email, you can't use this email", HttpStatus.BAD_REQUEST);
         }
         if (cliente.getClaveIngreso().isEmpty() ) {
-            return new ResponseEntity<>("Missing Password", HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>("Ingrese Password", HttpStatus.BAD_REQUEST);
         }
         if (servicioCliente.findByEmail(cliente.getEmail()) !=  null) {
-            return new ResponseEntity<>("Email already in use", HttpStatus.FORBIDDEN);
+            return new ResponseEntity<>("Email ya registrado", HttpStatus.FORBIDDEN);
         }
 
         servicioCliente.registrarCliente(cliente);
