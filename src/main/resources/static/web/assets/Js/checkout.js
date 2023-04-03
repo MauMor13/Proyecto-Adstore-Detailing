548aa39349472162d8470763d928222bb08debc7
unknown <juanbianchi@icloud.com>
2023-04-02 21:49:33 -0300

    cambios date

--- src/main/resources/static/web/assets/Js/checkout.js
+++ src/main/resources/static/web/assets/Js/checkout.js
@@ -7,7 +7,8 @@ createApp({
             productos: [],
             compra:{
                 productos:[],
-                servicios:[]
+                servicios:[],
+                total: 0,
             },
             cuentaCliente:false,
             errorEncontrado: false,
@@ -35,6 +36,12 @@ createApp({
             cvv:null,
             opcionPago:"",
             descontarAdstore:"",
+
+            fechasOcupadas: null,
+            disabledDatesArray: null,
+            selectedDate: null,
+            selectedHour: null,
+            isoString: null,
         }
     },
 
@@ -47,13 +54,55 @@ createApp({
         this.cargarDatos()
         this.cargarDatosServicios()
         this.cargarDatosLocalStorage()
-
+       // this.traerFechasOcupadas();
     },
 
     mounted(){
         this.compra = JSON.parse(localStorage.getItem("compra"))
         console.log(this.compra);
+
+        //FECHAS
+        //this.disabledDatesArray = this.fechasOcupadas.map((dateString) => new Date(dateString));
+        let picker = new AppointmentPicker(document.getElementById('time'), {
+            interval: 60,
+            mode: '12h',
+            minTime: 9,
+            maxTime: 20,
+            startTime: 9,
+            endTime: 21,
+            disabled: ['16:00', '17:00'],
+            title: 'Seleccione un horario',
+
+        });
+        const vm = this; // save reference to Vue instance
+
+        document.body.addEventListener('change.appo.picker', function (e) {
+            vm.selectedHour = e.displayTime;
+            vm.isoDate();
+        }, false);
+
+        config = {
+            locale: "es",
+            altInput: true,
+            altFormat: "F  j, Y  ",
+            minDate: "today",
+            maxDate: new Date().fp_incr(20),
+        }
+        flatpickr(document.getElementById('flatpickr'), config);
     },
+
+    updated(){
+        console.log("object");
+        config = {
+            locale: "es",
+            altInput: true,
+            altFormat: "F  j, Y  ",
+            minDate: "today",
+            maxDate: new Date().fp_incr(20),
+        }
+        flatpickr(document.getElementById('flatpickr'), config);
+    },
+
 
     methods:{
   
@@ -202,6 +251,12 @@ createApp({
                                 confirmButtonColor: '#f7ba24',
                                 confirmButtonText: 'Aceptar'
                             }).then((result) => {
+                                this.compra={
+                                    productos:[],
+                                    servicios:[],
+                                    total: 0,
+                                };
+                                localStorage.setItem("compra", JSON.stringify(this.compra))
                                 window.location.href = "/web/index.html"
                             })
                         })
@@ -219,6 +274,44 @@ createApp({
             })
         },
 
+
+        //FECHAS
+        traerFechasOcupadas() {
+            axios.get('/api/fechas-ocupadas')
+                .then(res => {
+                    this.fechasOcupadas = res
+                    console.log(this.fechasOcupadas)
+                })
+        },
+
+        log(event) {
+            console.log(event)
+            console.log(document.getElementById('time').addEventListener('change.appo.picker', function (e) { }),
+                document.getElementById('time').addEventListener('close.appo.picker', function (e) { }),
+                document.getElementById('time').addEventListener('open.appo.picker', function (e) { }))
+        },
+        updateSelectedDate(event) {
+            this.selectedDate = event.target.value;
+            console.log(this.selectedDate)
+        },
+        updateSelectedHour(event) {
+            this.selectedHour = event.target.value;
+            console.log(this.selectedHour)
+        },
+        isoDate() {
+            let combinedStr = this.selectedDate + ' ' + this.selectedHour;
+            let datetime = new Date(combinedStr);
+            this.isoString = datetime.toISOString();
+            console.log(this.isoString)
+        },
+
+    },
+
+    computed: {
+        flatpickrInstance() {
+            return flatpickr(this.$refs.datepicker)
+                .set('onChange', this.updateSelectedDate);
+        }
     },
 
 }).mount("#app")
